3e1f814b6e7e34a77ec9ab848687c6be0c434e29
JoshuaLight <j.light.developer@gmail.com>
2021-06-12 19:47:24 +0300

    Add new `polybar` config

--- polybar/forest/launch.sh
+++ polybar/forest/launch.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Add this script to your wm startup file.
+
+DIR="$HOME/.config/polybar/forest"
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,7 +10,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar main &
-
-echo "Polybar launched..."
+# Launch the bar
+polybar -q main -c "$DIR"/config.ini &
